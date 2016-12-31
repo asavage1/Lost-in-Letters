234a3d770c605809ece3f0a731f770d190207b9e
Andrew Savage <Andrew@Andrews-MacBook-Pro-70.local>
2016-12-30 19:11:55 -0500

    baby steps

---
+++ map.py
@@ -0,0 +1,16 @@
+#
+# Andrew Savage
+# map generation for lost in letters
+
+# Global variables for display size
+dispWidth = 30
+disHeight = 30
+
+# Run all processes to generate intial map
+def generateMap():
+    # Generate intia
+
+# Expand map when player goes outside of generate range
+def expandMap(): # Pass character coordinates?
+    # Add a chunk, determined by 
+    
