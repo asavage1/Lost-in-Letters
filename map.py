047b08dfb1ad4c4782e1fd1a61529340b3942e1f
Andrew Savage <andrew.savage.270@gmail.com>
2016-12-30 22:01:23 -0500

    Map file structure

--- map.py
+++ map.py
@@ -8,9 +8,26 @@ disHeight = 30
 
 # Run all processes to generate intial map
 def generateMap():
-    # Generate intia
+    # Determine initial mapsize
+    
+    # Declare 2d array for map: Option 1: One 2d array that can expand any
+    #                                     way but up
+    #                                  2: 2 2d arrays that can expand only 2
+    #                                     ways, both expand down, the left
+    #                                     one expands "left" and the right
+    #                                     one expands "right"
+    # Option 1 may make it hard to keep track of player coordinates
 
-# Expand map when player goes outside of generate range
+    # Initialize elements of array:
+    #    1. Initialize sky (spaces)
+    #    2. Initialize surface layer
+    #    3. Initialize other layers (TBD)
+    #    4. Initialize special objects (Later versions)
+
+    
+
+    
+# Expand map when player goes outside of generated range
 def expandMap(): # Pass character coordinates?
-    # Add a chunk, determined by 
+    # Add a chunk, size determined by global vairables
     
